0e107d9002370145e18d7686a3ed336da70c1c38
mikeshiwenbo <1281867218@qq.com>
2017-08-31 22:51:26 +0800

    1

---
+++ app/src/main/java/com/example/pc/kuoweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.example.pc.kuoweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by pc on 2017/8/31.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
